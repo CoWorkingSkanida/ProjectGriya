8a11e3c95301877a7276dd959ac14b7be8989477
Co Working Skanida <coworkingkskanida@gmail.com>
2023-02-22 08:48:54 +0700

    p

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,22 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
-  styleUrls: ['./test.component.scss']
+  styleUrls: ['./test.component.scss'],
 })
-export class TestComponent implements OnInit{
-  slides: any[] = new Array(3).fill({id: -1, src: '', title: '', subtitle: ''});
+export class TestComponent implements OnInit {
+  slides: any[] = new Array(3).fill({
+    id: -1,
+    src: '',
+    title: '',
+    subtitle: '',
+  });
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.slides[0] = {
       src: '../assets/pexels-christian-heitz-842711 1 (1).png',
     };
     this.slides[1] = {
-      src:  '../assets/pexels-christian-heitz-842711 1 (1).png',
-    }
+      src: '../assets/pexels-christian-heitz-842711 1 (1).png',
+    };
     this.slides[2] = {
-      src:  '../assets/pexels-christian-heitz-842711 1 (1).png',
-    }
+      src: '../assets/pexels-christian-heitz-842711 1 (1).png',
+    };
   }
-} 
+}
