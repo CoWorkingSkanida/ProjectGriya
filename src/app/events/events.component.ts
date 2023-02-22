6a0098ee0f8a2eadeaa50c33bef91bcdf9dec445
CoWorkingSkanida <coworkingkskanida@gmail.com>
2023-02-22 09:35:35 +0700

    events screen

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./events.component.scss']
 })
 export class EventsComponent {
+  slides: any[] = new Array(3).fill({id: -1, src: '', title: '', subtitle: ''});
 
-}
+  constructor() { }
+
+  ngOnInit(): void {
+    this.slides[0] = {
+      src: '../assets/pexels-christian-heitz-842711 1 (1).png',
+    };
+    this.slides[1] = {
+      src:  '../assets/pexels-christian-heitz-842711 1 (1).png',
+    }
+    this.slides[2] = {
+      src:  '../assets/pexels-christian-heitz-842711 1 (1).png',
+    }
+  }
+} 
