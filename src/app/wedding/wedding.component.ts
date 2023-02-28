08c4d1c73de467d2a4328a0e750c765deea41ddb
Co Working Skanida <coworkingkskanida@gmail.com>
2023-02-28 18:56:45 +0700

    booking

--- src/app/wedding/wedding.component.ts
+++ src/app/wedding/wedding.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-wedding',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./wedding.component.scss']
 })
 export class WeddingComponent {
+  slides: any[] = new Array(3).fill({
+    id: -1,
+    src: '',
+    title: '',
+    subtitle: '',
+  });
 
+  isReadmore = true;
+  isLearnbuy = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.slides[0] = {
+      src: '../assets/komputer 2.png',
+    };
+    this.slides[1] = {
+      src: '../assets/komputer 2.png',    };
+    this.slides[2] = {
+      src: '../assets/komputer 2.png',    };
+  }
+
+  showText() {
+    this.isReadmore = !this.isReadmore;
+  }
+
+  learnBuy() {
+    this.router.navigateByUrl('/booking');
+  }
 }
