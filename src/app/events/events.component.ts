fe0117f24306769b7fc54919a25cfa757657964a
CoWorkingSkanida <coworkingkskanida@gmail.com>
2023-04-10 09:56:37 +0700

    cont1

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -12,13 +12,16 @@ export class EventsComponent {
 
   ngOnInit(): void {
     this.slides[0] = {
-      src: '../assets/img/pexels-christian-heitz-842711 1 (1).png',
+      src: '../assets/img/1.png',
     };
     this.slides[1] = {
-      src:  '../assets/img/pexels-christian-heitz-842711 1 (1).png',
+      src:  '../assets/img/m2.png',
     }
     this.slides[2] = {
-      src:  '../assets/img/pexels-christian-heitz-842711 1 (1).png',
+      src:  '../assets/img/m3.png',
+    }
+    this.slides[3] = {
+      src:  '../assets/img/5.png',
     }
   }
 } 
